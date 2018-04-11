a0f3acbb02b210800f534828fd57a736538d92b4
dm2002 <774681508@qq.com>
2018-04-11 17:48:10 +0800

    start

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <div id="app">
-    <img src="@/assets/imgs/logo.png">
-    <router-view/>
+   <router-view/>
   </div>
 </template>
 
 <script>
-import {fontSize} from '@/assets/js/common.js'
-
 export default {
   name: 'App',
   created(){
-    fontSize(15);
   }
-
 }
 </script>
 
--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -6,11 +6,4 @@ function localStorageFetch(key) {
 	return JSON.parse(localStorage.getItem(key)) || {}
 }
 
-//define font-size
-function fontSize(value) {
-	let html = document.documentElement;
-	let hWidth = html.getBoundingClientRect().width;
-	html.style.fontSize = hWidth / value + "px";
-}
-
-export {localStorageSave,localStorageFetch,fontSize}
+export {localStorageSave,localStorageFetch}
